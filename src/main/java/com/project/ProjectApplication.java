aa80bf5eeeda8e32052bcc2ee856312bbb4686ec
MrXT <a690215719@163.com>
2017-02-08 16:26:10 +0800

    2222
    2222

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -51,6 +51,7 @@ public class ProjectApplication extends WebMvcConfigurerAdapter{
     }
     
     public static void main(String[] args) throws Exception {
+        System.out.println(11);
         SpringApplication.run(ProjectApplication.class, args);
     }
     /**
