0407a086a87cac2d90960882797c7c17a01ff7e0
MrXT <a690215719@163.com>
2017-02-08 17:20:29 +0800

    Update ProjectApplication.java

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -65,7 +65,6 @@ public class ProjectApplication extends WebMvcConfigurerAdapter{
     @ApiIgnore
     public void index(HttpServletResponse response) throws IOException{
         response.sendRedirect("login/main");
-        System.out.println("解决冲突文件");
     }
     /**
      * 配置文件上传bean
