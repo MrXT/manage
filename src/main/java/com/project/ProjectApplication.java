047d10c7ef5b928247256bfa2dc7747388ab9877
xiaoti <di.xiao@karakal.com.cn>
2017-02-08 17:04:15 +0800

    1111

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -52,7 +52,9 @@ public class ProjectApplication extends WebMvcConfigurerAdapter{
     
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ProjectApplication.class, args);
-
+    }
+    public static void main1(String[] args) throws Exception {
+        SpringApplication.run(ProjectApplication.class, args);
     }
     /**
      * 页面跳转
