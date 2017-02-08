30a6a2df4ebde79a914ef5d5bfc0fa9ac59febbb
MrXT <a690215719@163.com>
2017-02-08 17:07:46 +0800

    Update ProjectApplication.java

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -56,9 +56,6 @@ public class ProjectApplication extends WebMvcConfigurerAdapter{
     public static void main1(String[] args) throws Exception {
         SpringApplication.run(ProjectApplication.class, args);
     }
-     public static void main2(String[] args) throws Exception {
-        SpringApplication.run(ProjectApplication.class, args);
-    }
     /**
      * 页面跳转
      * @param response
